1ac2606e114a2431a6c5c3bbc549b9b33840b377
ssantamariaparra <ssantamariaparra@gmail.com>
2014-01-22 14:29:53 -0500

    Phonegap helloworld app

---
+++ platforms/android/CordovaLib/gen/R.java.d
@@ -0,0 +1,1 @@
+ : D:\Development Projects\helloworld\platforms\android\CordovaLib\bin\AndroidManifest.xml \
---
+++ platforms/android/gen/R.java.d
@@ -0,0 +1,9 @@
+D:\Development Projects\helloworld\platforms\android\gen\com\phonegap\helloworld\R.java \
+ : D:\Development Projects\helloworld\platforms\android\res\drawable\icon.png \
+D:\Development Projects\helloworld\platforms\android\res\drawable-hdpi\icon.png \
+D:\Development Projects\helloworld\platforms\android\res\drawable-ldpi\icon.png \
+D:\Development Projects\helloworld\platforms\android\res\drawable-mdpi\icon.png \
+D:\Development Projects\helloworld\platforms\android\res\drawable-xhdpi\icon.png \
+D:\Development Projects\helloworld\platforms\android\res\values\strings.xml \
+D:\Development Projects\helloworld\platforms\android\res\xml\config.xml \
+D:\Development Projects\helloworld\platforms\android\bin\AndroidManifest.xml \
